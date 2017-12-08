de0f3cbd5ffb8638040314cebbf608124e1937ec
winson <21535474@qq.com>
2017-12-08 20:22:17 +0800

    project init commit

---
+++ src/main/java/com/windmall/dao/OrderItemMapper.java
@@ -0,0 +1,17 @@
+package com.windmall.dao;
+
+import com.windmall.pojo.OrderItem;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
---
+++ src/main/java/com/windmall/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.windmall.dao;
+
+import com.windmall.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
